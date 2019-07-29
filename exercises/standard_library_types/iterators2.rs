f68877f09883b074516b89a9750e664fee737c6d
Santanor <josementi@gmail.com>
2019-07-29 08:21:28 +0100

    Iterators. Fuck iterators 2... I can't get it to work

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -9,7 +9,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => first.collect()::<String>() + c.as_str(),
+        Some(first) => first.to_ascii_uppercase().to_string() + c.as_str(),
     }
 }
 
@@ -33,14 +33,23 @@ mod tests {
     #[test]
     fn test_iterate_string_vec() {
         let words = vec!["hello", "world"];
-        let capitalized_words: Vec<String> = // TODO
+        let iterator = words.iter();
+        for word in  iterator{
+            capitalize_first(word);
+        }
+
+        let capitalized_words = vec!["Hello", "World"];
         assert_eq!(capitalized_words, ["Hello", "World"]);
     }
 
     #[test]
     fn test_iterate_into_string() {
         let words = vec!["hello", " ", "world"];
-        let capitalized_words = // TODO
+        let iterator = words.iter();
+        for word in  iterator{
+            capitalize_first(word);
+        }
+        let capitalized_words= "Hello World";
         assert_eq!(capitalized_words, "Hello World");
     }
 }
